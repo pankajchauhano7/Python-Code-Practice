05eb7b5c70c899d383a00e6f9bf92070e3e195f3
Pankaj Chauhan <pcpankajo7@gmail.com>
2025-01-04 14:43:49 +0530

    changes in nonRepeatCharList.py

--- nonRepeatCharList.py
+++ nonRepeatCharList.py
@@ -1,13 +1,13 @@
 str=["swiss","abcabcd","abcd"]
 non_repchar=[]
+char_count={}
 for s in str:
-    char_count={}
     for c in s:
         if c in char_count:
             char_count[c] += 1
         else:
             char_count[c] = 1
-    for c,freq in char_count.items():
-           if freq == 1:
-                non_repchar.append(c)
+for c,freq in char_count.items():
+      if freq == 1:
+        non_repchar.append(c)
 print(non_repchar)
