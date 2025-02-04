fd41ccfd6b6b8c0e59f48d5b4686ecf4b40f1a7a
Pankaj Chauhan <pcpankajo7@gmail.com>
2025-02-05 00:29:09 +0530

    Second highest from duplicate values in list

--- secondHigDupList.py
+++ secondHigDupList.py
@@ -1,3 +1,4 @@
+#Find Second highest number from duplicate values list
 arr = [23, 45, 1, 5, 70, 45, 12, 70, 1, 33, 45]
 arr = list(set(arr))  # Remove duplicates
 arr.sort(reverse=True)  # Sort in descending order
