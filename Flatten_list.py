fd9c0b786487acdc10c1a4a2655e2e88b763b34c
pankajchauhano7 <pcpankajo7@gmail.com>
2025-12-22 19:19:16 +0530

    flatten the list

---
+++ Flatten_list.py
@@ -0,0 +1,15 @@
+a=[1,[2,[3,4],6]]
+
+
+def flatten(a):
+    new_a=[]
+    for i in a:
+        if isinstance(i,list):
+            new_a.extend(flatten(i))
+        else:
+            new_a.append(i)
+    return new_a
+
+
+print(flatten(a))
+
